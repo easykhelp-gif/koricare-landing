3e5b405b936d8b57a6e412381e0961eb9bb1a732
y1611 <y1611@example.com>
2026-05-04 14:27:46 +0900

    Update LINE links to real ID and hide Facebook buttons for launch

--- components/CTASection.tsx
+++ components/CTASection.tsx
@@ -93,7 +93,7 @@ export default function CTASection() {
         <div style={{ display: "flex", flexDirection: "column", gap: 12, position: "relative" }}>
           <a
             id="cta-line-btn"
-            href="https://line.me/ti/g2/YOUR_LINE_LINK"
+            href="https://line.me/R/ti/p/@768mkjml"
             target="_blank"
             rel="noopener noreferrer"
             className="btn-shimmer"
@@ -124,7 +124,8 @@ export default function CTASection() {
             คุยกับเพื่อน Easy-K ผ่าน LINE
           </a>
 
-          <a
+          {/* Facebook button hidden until ready */}
+          {/* <a
             id="cta-fb-btn"
             href="https://www.facebook.com/YOUR_FB_PAGE"
             target="_blank"
@@ -155,7 +156,7 @@ export default function CTASection() {
               <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
             </svg>
             ติดต่อผ่าน Facebook Messenger
-          </a>
+          </a> */}
         </div>
 
         {/* Fine print */}
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -137,7 +137,7 @@ export default function HeroSection() {
         >
           <a
             id="hero-line-btn"
-            href="https://line.me/ti/g2/YOUR_LINE_LINK"
+            href="https://line.me/R/ti/p/@768mkjml"
             target="_blank"
             rel="noopener noreferrer"
             style={{
@@ -168,7 +168,8 @@ export default function HeroSection() {
             คุยกับเพื่อน Easy-K ผ่าน LINE
           </a>
 
-          <a
+          {/* Facebook button hidden until ready */}
+          {/* <a
             id="hero-fb-btn"
             href="https://www.facebook.com/YOUR_FB_PAGE"
             target="_blank"
@@ -200,7 +201,7 @@ export default function HeroSection() {
               <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
             </svg>
             ติดตามเราบน Facebook
-          </a>
+          </a> */}
         </div>
 
         {/* Scroll hint */}
